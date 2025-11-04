90f335017ac2460a622a0c429d1cc6f48aba97df
TranXuanTruong205 <truong2005tran@gmail.com>
2025-11-04 23:56:31 +0700

    Initial project structure for DINERestaurant (Android + Spring Boot)

---
+++ DINERestaurant-Backend/src/main/java/com/dine/DINERestaurant_Backend/DineRestaurantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dine.DINERestaurant_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineRestaurantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineRestaurantBackendApplication.class, args);
+	}
+
+}
---
+++ DINERestaurant-Backend/src/test/java/com/dine/DINERestaurant_Backend/DineRestaurantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dine.DINERestaurant_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineRestaurantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
