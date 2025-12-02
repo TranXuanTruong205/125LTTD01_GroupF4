551b10d3ea50412f78431c73e21991b98512090f
DESKTOP-V3OMU8B\LEGION <minhtri.lmt2402@gmail.com>
2025-12-03 04:23:58 +0700

    feat: implement Category API (Entity, Repository, Service, Controller)

---
+++ DINERestaurant-Backend/src/main/java/com/dine/DINERestaurant_Backend/menu/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.dine.DINERestaurant_Backend.menu.repository;
+import com.dine.DINERestaurant_Backend.menu.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    
+}
