be91a6722ab954252c295a48ce6e10facac5ee8f
TranXuanTruong205 <truong2005tran@gmail.com>
2025-12-07 23:29:42 +0700

    fix CartController.java and .gitignore

--- DINERestaurant-Backend/src/main/java/com/dine/DINERestaurant_Backend/cart/controller/CartController.java
+++ DINERestaurant-Backend/src/main/java/com/dine/DINERestaurant_Backend/cart/controller/CartController.java
@@ -4,7 +4,6 @@ import com.dine.DINERestaurant_Backend.cart.entity.Cart;
 import com.dine.DINERestaurant_Backend.cart.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
