e77aa739fde19f5f45b8bd5e2cb07d562b460e8c
TranXuanTruong205 <truong2005tran@gmail.com>
2025-12-08 22:44:39 +0700

    feat(auth): implement verification feature ui

--- DINERestaurant-App/app/src/main/java/com/dinerestaurant/app/data/remote/api/ApiClient.java
+++ DINERestaurant-App/app/src/main/java/com/dinerestaurant/app/data/remote/api/ApiClient.java
@@ -1,34 +1,35 @@
 package com.dinerestaurant.app.data.remote.api;
 
+import android.content.Context;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiClient {
 
-    private static final String BASE_URL = "http://10.0.2.2:8080/"; // emulator
-
     private static Retrofit retrofit;
+    private static final String BASE_URL = "http://10.0.2.2:8080/";
 
-    public static Retrofit getClient() {
-        if (retrofit == null) {
-            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+    public static void init(Context context) {
+        if (retrofit != null) return;
 
-            OkHttpClient client = new OkHttpClient.Builder()
-                    .addInterceptor(logging)
-                    .build();
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .client(client)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-        }
-        return retrofit;
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new AuthInterceptor(context))
+                .addInterceptor(logging)
+                .build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
     }
 
     public static AuthApi getAuthApi() {
-        return getClient().create(AuthApi.class);
+        return retrofit.create(AuthApi.class);
     }
 }
